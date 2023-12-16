bafb5d920376ef4f73e47b3f99cc1597bb774c9b
SUBHOSHREE PATTNAIK <76872340+subhoshreep@users.noreply.github.com>
2023-12-16 15:24:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,4 +50,24 @@ let changeText = () => {
 };
 changeText();
 setInterval(changeText,3000);
+////////////////////sticky navbar//////////////////////
+
+const header=document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY>50)
+})
+
+// //////////navlistmenudrop//////////////////////
+let menuIcon=document.querySelector("#menu-icon");
+let navlist=document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+// window.onscroll=()=>{
+//     menuIcon.classList.remove("bx-x");
+//     navlist.classList.replace("open");
+// }
 
